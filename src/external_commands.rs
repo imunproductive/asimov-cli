6c431ed5cdf7197e41b0611982035ed7c18d42bf
imunproductive <yurii@asimov.so>
2025-02-26 08:05:44 +0200

    implement new collecting

--- src/external_commands.rs
+++ src/external_commands.rs
@@ -1,6 +1,6 @@
 // This is free and unencumbered software released into the public domain.
 
-use std::{fs::ReadDir, path::PathBuf};
+use std::path::{Path, PathBuf};
 
 pub type Result<T> = std::io::Result<T>;
 
@@ -22,37 +22,24 @@ impl ExternalCommands {
     }
 
     fn collect_internal(prefix: &str, level: usize) -> Result<Vec<ExternalCommand>> {
-        let Some(paths) = std::env::var_os("PATH") else {
-            // PATH variable is not set.
-            return Ok(vec![]);
-        };
+        let result = Self::collect_commands(prefix)?
+            .into_iter()
+            // Construct ExternalCommand.
+            .flat_map(|path| {
+                let name = path
+                    .file_stem()?
+                    .to_string_lossy()
+                    .trim_start_matches(prefix)
+                    .to_string();
 
-        let mut result = vec![];
-        for path in std::env::split_paths(&paths) {
-            let Ok(dir) = std::fs::read_dir(path) else {
-                continue;
-            };
-
-            let commands = Self::collect_in_dir(prefix, dir)?
-                .into_iter()
-                // Construct ExternalCommand.
-                .flat_map(|path| {
-                    let name = path
-                        .file_stem()?
-                        .to_string_lossy()
-                        .trim_start_matches(prefix)
-                        .to_string();
-
-                    Some(ExternalCommand { name, path })
-                })
-                // Respect level.
-                .filter(|cmd| {
-                    let count = cmd.name.chars().filter(|&c| c == '-').count();
-                    count < level
-                });
-
-            result.extend(commands);
-        }
+                Some(ExternalCommand { name, path })
+            })
+            // Respect level.
+            .filter(|cmd| {
+                let count = cmd.name.chars().filter(|&c| c == '-').count();
+                count < level
+            })
+            .collect();
 
         Ok(result)
     }
@@ -70,109 +57,214 @@ impl ExternalCommands {
 
 #[cfg(unix)]
 impl ExternalCommands {
-    fn is_executable_metadata(metadata: std::fs::Metadata) -> bool {
+    fn filter_file(prefix: &str, path: &Path) -> bool {
         use std::os::unix::prelude::*;
-        metadata.is_file() && metadata.permissions().mode() & 0o111 != 0
+
+        let file_name = path.file_name();
+        let Some(entry_name) = file_name.and_then(|name| name.to_str()) else {
+            // skip files with invalid names.
+            return false;
+        };
+
+        if entry_name.starts_with(".") || entry_name.ends_with("~") {
+            // skip hidden and backup files.
+            return false;
+        }
+
+        if !entry_name.starts_with(prefix) {
+            // skip non-matching files.
+            return false;
+        }
+
+        let Ok(metadata) = std::fs::metadata(path) else {
+            // couldn't get metadata.
+            return false;
+        };
+
+        if !metadata.is_file() || metadata.permissions().mode() & 0o111 == 0 {
+            // skip non-executable files.
+            return false;
+        }
+
+        true
     }
 
-    fn collect_in_dir(prefix: &str, dir: ReadDir) -> Result<Vec<PathBuf>> {
+    fn collect_commands(prefix: &str) -> Result<Vec<PathBuf>> {
+        let Some(paths) = std::env::var_os("PATH") else {
+            // PATH variable is not set.
+            return Ok(vec![]);
+        };
+
         let mut result = vec![];
-
-        for entry in dir {
-            let Ok(entry) = entry else {
-                // invalid entry.
+        for path in std::env::split_paths(&paths) {
+            let Ok(dir) = std::fs::read_dir(path) else {
                 continue;
             };
 
-            let file_name = entry.file_name();
-            let Some(entry_name) = file_name.to_str() else {
-                // skip files with invalid names.
-                continue;
-            };
+            for entry in dir {
+                let Ok(entry) = entry else {
+                    // invalid entry.
+                    continue;
+                };
 
-            if entry_name.starts_with(".") || entry_name.ends_with("~") {
-                // skip hidden and backup files.
-                continue;
+                let path = entry.path();
+                if Self::filter_file(prefix, &path) {
+                    result.push(path);
+                }
             }
-
-            if !entry_name.starts_with(prefix) {
-                // skip non-matching files.
-                continue;
-            }
-
-            let Ok(metadata) = entry.metadata() else {
-                // couldn't get metadata.
-                continue;
-            };
-
-            if !Self::is_executable_metadata(metadata) {
-                // skip non-executable files.
-                continue;
-            }
-
-            result.push(entry.path());
         }
 
         Ok(result)
+    }
+
+    fn resolve_command(prefix: &str, command: &str) -> Option<PathBuf> {
+        let Some(paths) = std::env::var_os("PATH") else {
+            // PATH variable is not set.
+            return None;
+        };
+
+        for path in std::env::split_paths(&paths) {
+            let path = path.join(command);
+
+            if !path.exists() {
+                continue;
+            }
+
+            if !Self::filter_file(prefix, &path) {
+                continue;
+            }
+
+            return Some(path);
+        }
+
+        None
     }
 }
 
 #[cfg(windows)]
 impl ExternalCommands {
-    fn collect_in_dir(prefix: &str, dir: ReadDir) -> Result<Vec<PathBuf>> {
-        use std::env::consts::EXE_EXTENSION;
-        use std::os::windows::fs::MetadataExt;
+    fn get_path_exts() -> Result<Vec<String>> {
+        let Ok(exts) = std::env::var("PATHEXT") else {
+            // PATHEXT variable is not set.
+            return Ok(vec![]);
+        };
 
+        // NOTE: I am not sure if std::env::split_paths should be applied here,
+        // since it also deals with '"' which seems to not be used in PATHEXT?
+        return Ok(exts
+            .split(';')
+            .map(|ext| ext[1..].to_lowercase())
+            .collect::<Vec<_>>());
+    }
+
+    fn filter_file(prefix: &str, path: &Path, exts: Option<&[String]>) -> bool {
+        use std::os::windows::fs::MetadataExt;
         const FILE_ATTRIBUTE_HIDDEN: u32 = 0x00000002;
 
-        // FIXME: Currently we don't handle the case when
-        // there are multiple files with the same name,
-        // but different extensions. This is a bug.
-        static BAT_EXTENSION: &str = "bat";
-        static CMD_EXTENSION: &str = "cmd";
-        static ALLOWED_EXTENSIONS: [&str; 3] = [BAT_EXTENSION, CMD_EXTENSION, EXE_EXTENSION];
+        let file_name = path.file_name();
+        let Some(entry_name) = file_name.and_then(|name| name.to_str()) else {
+            // skip files with invalid names.
+            return false;
+        };
 
-        let mut result = vec![];
+        if !entry_name.starts_with(prefix) {
+            // skip non-matching files.
+            return false;
+        }
 
-        for entry in dir {
-            let Ok(entry) = entry else {
-                // invalid entry.
-                continue;
-            };
-
-            let file_name = entry.file_name();
-            let Some(entry_name) = file_name.to_str() else {
-                // skip files with invalid names.
-                continue;
-            };
-
-            if !entry_name.starts_with(prefix) {
-                // skip non-matching files.
-                continue;
-            }
-
-            let entry_path = entry.path();
-            let Some(entry_ext) = entry_path.extension().and_then(|ext| ext.to_str()) else {
+        if let Some(exts) = exts {
+            let Some(entry_ext) = path.extension().and_then(|ext| ext.to_str()) else {
                 // skip files without extensions.
-                continue;
+                return false;
             };
 
             let entry_ext = entry_ext.to_lowercase();
-            if !ALLOWED_EXTENSIONS.contains(&entry_ext.as_str()) {
+            if !exts.contains(&entry_ext) {
                 // skip non-executable files
-                continue;
+                return false;
             }
+        }
 
-            let metadata = entry.metadata()?;
-            let is_hidden = metadata.file_attributes() & FILE_ATTRIBUTE_HIDDEN != 0;
-            if is_hidden {
-                // skip hidden files
+        let Ok(metadata) = std::fs::metadata(path) else {
+            // couldn't get metadata.
+            return false;
+        };
+
+        let is_hidden = metadata.file_attributes() & FILE_ATTRIBUTE_HIDDEN != 0;
+        if is_hidden {
+            // skip hidden files
+            return false;
+        }
+
+        true
+    }
+
+    fn collect_commands(prefix: &str) -> Result<Vec<PathBuf>> {
+        let Some(paths) = std::env::var_os("PATH") else {
+            // PATH variable is not set.
+            return Ok(vec![]);
+        };
+
+        let Ok(exts) = Self::get_path_exts() else {
+            // PATHEXT variable is not set or invalid.
+            return Ok(vec![]);
+        };
+
+        let mut result = vec![];
+        for path in std::env::split_paths(&paths) {
+            let Ok(dir) = std::fs::read_dir(path) else {
                 continue;
-            }
+            };
 
-            result.push(entry_path);
+            for entry in dir {
+                let Ok(entry) = entry else {
+                    // invalid entry.
+                    continue;
+                };
+
+                let path = entry.path();
+                if Self::filter_file(prefix, &path, Some(&exts)) {
+                    result.push(path);
+                }
+            }
         }
 
         Ok(result)
+    }
+
+    fn resolve_command(prefix: &str, command: &str) -> Option<PathBuf> {
+        let Some(paths) = std::env::var_os("PATH") else {
+            // PATH variable is not set.
+            return None;
+        };
+
+        let Ok(exts) = Self::get_path_exts() else {
+            // PATHEXT variable is not set or invalid.
+            return None;
+        };
+
+        for path in std::env::split_paths(&paths) {
+            let mut path = path.join(command);
+
+            // Extension is provided. Just check if file exists.
+            if path.extension().is_some() {
+                match path.exists() {
+                    true if Self::filter_file(prefix, &path, None) => return Some(path),
+                    _ => continue,
+                }
+            }
+
+            // Iterate extensions and check if file exists.
+            for ext in &exts {
+                path.set_extension(ext);
+
+                match path.exists() {
+                    true if Self::filter_file(prefix, &path, None) => return Some(path),
+                    _ => continue,
+                }
+            }
+        }
+
+        None
     }
 }
