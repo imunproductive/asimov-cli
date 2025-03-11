646b4a3d383b7507c17fa8366c8fabec5b505fdf
Yurii Antoniuk <yurii@asimov.so>
2025-03-11 14:32:21 +0200

    Add Windows support and tests. (#9)

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+// This is free and unencumbered software released into the public domain.
+
+mod external_commands;
+pub use external_commands::*;
--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,9 @@ use clientele::{
     exit, StandardOptions,
     SysexitsError::*,
 };
-use std::{
-    collections::BTreeSet,
-    env::consts::EXE_SUFFIX,
-    path::{Path, PathBuf},
-    process::Stdio,
-};
+use std::process::Stdio;
 
-#[cfg(unix)]
-use std::fs::Metadata;
+use asimov_cli::ExternalCommands;
 
 /// ASIMOV Command-Line Interface (CLI)
 #[derive(Debug, Parser)]
@@ -67,44 +61,47 @@ pub fn main() {
         exit(EX_OK);
     }
 
-    // Print the help message, if requested:
-    if options.help {
-        let mut help = String::new();
-        help.push_str(color_print::cstr!("<s><u>Commands:</u></s>\n"));
-        let subcommands = find_external_subcommands("asimov-", 1).unwrap_or_default();
-        for (i, subcommand_name) in subcommands.iter().enumerate() {
-            if i > 0 {
-                help.push('\n');
-            }
-            help.push_str(&color_print::cformat!(
-                "\t<dim>$</dim> <s>asimov {}</s> [OPTIONS] [COMMAND]",
-                subcommand_name,
-            ));
-        }
-        Options::command()
-            .after_long_help(help)
-            .print_long_help()
-            .unwrap();
-        exit(EX_OK);
-    }
-
     // Configure debug output:
     if options.flags.debug {
         std::env::set_var("RUST_BACKTRACE", "1");
     }
 
+    // Print the help message, if requested:
+    if options.help {
+        let mut help = String::new();
+        help.push_str(color_print::cstr!("<s><u>Commands:</u></s>\n"));
+
+        let commands = ExternalCommands::collect("asimov-", 1);
+        for (i, cmd) in commands.iter().enumerate() {
+            if i > 0 {
+                help.push('\n');
+            }
+            help.push_str(&color_print::cformat!(
+                "\t<dim>$</dim> <s>asimov {}</s> [OPTIONS] [COMMAND]",
+                cmd.name,
+            ));
+        }
+
+        Options::command()
+            .after_long_help(help)
+            .print_long_help()
+            .unwrap();
+
+        exit(EX_OK);
+    }
+
     // Locate the given subcommand:
     let Command::External(command) = &options.command.unwrap();
-    let Some(command_path) = find_external_subcommand("asimov-", &command[0]) else {
+    let Some(cmd_to_execute) = ExternalCommands::find("asimov-", &command[0]) else {
         eprintln!(
-            "{}: command not found: {}{}{}",
-            "asimov", "asimov-", command[0], EXE_SUFFIX
+            "{}: command not found: {}{}",
+            "asimov", "asimov-", command[0]
         );
         exit(EX_UNAVAILABLE);
     };
 
     // Execute the given subcommand:
-    let status = std::process::Command::new(command_path)
+    let status = std::process::Command::new(&cmd_to_execute.path)
         .args(&command[1..])
         .stdin(Stdio::inherit())
         .stdout(Stdio::inherit())
@@ -136,117 +133,4 @@ pub fn main() {
             exit(status.code().unwrap_or(EX_SOFTWARE.as_i32()))
         }
     }
-}
-
-fn find_external_subcommands(prefix: &str, level: usize) -> std::io::Result<BTreeSet<String>> {
-    Ok(find_external_commands(prefix, level)?
-        .iter()
-        .filter_map(|path| {
-            path.with_extension("").file_name().map(|name| {
-                name.to_string_lossy()
-                    .trim_start_matches(prefix)
-                    .to_string()
-            })
-        })
-        .collect())
-}
-
-fn find_external_commands(prefix: &str, level: usize) -> std::io::Result<Vec<PathBuf>> {
-    let prefix = prefix.as_bytes();
-    let mut result = vec![];
-    let Some(paths) = std::env::var_os("PATH") else {
-        return Ok(result);
-    };
-    for path in std::env::split_paths(&paths) {
-        let Ok(dir) = std::fs::read_dir(path) else {
-            continue;
-        };
-
-        #[cfg(windows)]
-        {
-            use std::env::consts::EXE_EXTENSION;
-            use std::os::windows::fs::MetadataExt;
-            const FILE_ATTRIBUTE_HIDDEN: u32 = 0x00000002;
-
-            for entry in dir {
-                let entry = entry?;
-                let entry_name = entry.file_name();
-                let entry_bytes = entry_name.as_encoded_bytes();
-                let entry_path = entry.path();
-                let entry_ext = entry_path.extension().and_then(|ext| ext.to_str());
-                if entry_ext != Some(EXE_EXTENSION) || !entry_bytes.starts_with(prefix) {
-                    continue; // skip non-executable and non-matching files
-                }
-
-                let metadata = entry.metadata()?; // system call
-                let is_hidden = metadata.file_attributes() & FILE_ATTRIBUTE_HIDDEN != 0;
-                if is_hidden {
-                    continue; // skip hidden files
-                }
-
-                let count = entry_bytes
-                    .iter()
-                    .fold(0, |sum, &b| sum + (b == b'-') as usize);
-                if count <= level {
-                    result.push(entry.path());
-                }
-            }
-        }
-
-        #[cfg(unix)]
-        {
-            use std::os::unix::ffi::OsStrExt;
-
-            for entry in dir {
-                let entry = entry?;
-                let entry_name = entry.file_name();
-                let entry_bytes = entry_name.as_bytes();
-                if entry_bytes.starts_with(b".")
-                    || entry_bytes.ends_with(b"~")
-                    || !entry_bytes.starts_with(prefix)
-                    || !is_executable_metadata(entry.metadata()?)
-                {
-                    continue; // skip hidden, backup, and non-executable files
-                }
-                let count = entry_bytes
-                    .iter()
-                    .fold(0, |sum, &b| sum + (b == b'-') as usize);
-                if count <= level {
-                    result.push(entry.path());
-                }
-            }
-        }
-    }
-    Ok(result)
-}
-
-fn find_external_subcommand(prefix: &str, subcommand: &str) -> Option<PathBuf> {
-    find_external_command(&format!("{}{}", prefix, subcommand))
-}
-
-fn find_external_command(command: &str) -> Option<PathBuf> {
-    let command_exe = format!("{}{}", command, EXE_SUFFIX);
-    std::env::var_os("PATH").and_then(|paths| {
-        std::env::split_paths(&paths)
-            .map(|path| path.join(&command_exe))
-            .find(|path| is_executable_path(path))
-    })
-}
-
-#[cfg(windows)]
-fn is_executable_path(path: impl AsRef<Path>) -> bool {
-    path.as_ref().is_file()
-}
-
-#[cfg(unix)]
-fn is_executable_path(path: impl AsRef<Path>) -> bool {
-    std::fs::metadata(path)
-        .map(is_executable_metadata)
-        .unwrap_or(false)
-}
-
-#[cfg(unix)]
-fn is_executable_metadata(metadata: Metadata) -> bool {
-    use std::os::unix::prelude::*;
-    metadata.is_file() && metadata.permissions().mode() & 0o111 != 0
 }
