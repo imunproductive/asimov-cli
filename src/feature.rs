43f3ff4ab592ee458936bc4508dcf6b60bf83eec
imunproductive <yurii@asimov.so>
2025-03-11 15:29:31 +0200

    temporary replace to "hello world"

--- src/feature.rs
+++
@@ -1,8 +0,0 @@
-// This is free and unencumbered software released into the public domain.
-
-/// The set of features that are enabled in this build of the program.
-#[allow(unused)]
-pub static FEATURES: &[&str] = &[
-    #[cfg(feature = "unstable")]
-    "unstable",
-];
--- src/lib.rs
+++
@@ -1,4 +0,0 @@
-// This is free and unencumbered software released into the public domain.
-
-mod external_commands;
-pub use external_commands::*;
