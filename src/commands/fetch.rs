6e1b816680bec0b2c8e8706f991973435c54e248
Arto Bendiken <arto@asimov.so>
2025-04-25 01:14:00 +0200

    Implement `asimov fetch`.

--- src/commands/fetch.rs
+++ src/commands/fetch.rs
@@ -1,7 +1,22 @@
 // This is free and unencumbered software released into the public domain.
 
 use crate::{StandardOptions, SysexitsError};
+use std::io::stdout;
 
-pub fn fetch(_urls: &Vec<String>, _flags: &StandardOptions) -> Result<(), SysexitsError> {
-    Err(SysexitsError::EX_UNAVAILABLE) // TODO
+pub fn fetch(urls: &Vec<String>, _flags: &StandardOptions) -> Result<(), SysexitsError> {
+    let mut output = stdout().lock();
+
+    for url in urls {
+        match asimov_proxy::open(url) {
+            Ok(mut input) => {
+                std::io::copy(&mut input, &mut output)?;
+            }
+            Err(error) => {
+                eprintln!("failed to fetch URL `{}`: {}", url, error);
+                return Err(error.into());
+            }
+        }
+    }
+
+    Ok(())
 }
