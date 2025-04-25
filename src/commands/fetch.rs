0ef11d01c11b05a7fc51ef432d1f09f243eea9ab
Arto Bendiken <arto@asimov.so>
2025-04-25 13:13:35 +0200

    Enhance `asimov fetch`.

--- src/commands/fetch.rs
+++ src/commands/fetch.rs
@@ -1,22 +1,57 @@
 // This is free and unencumbered software released into the public domain.
 
 use crate::{StandardOptions, SysexitsError};
+use color_print::ceprintln;
+use miette::{IntoDiagnostic, Result};
 use std::io::stdout;
 
-pub fn fetch(urls: &Vec<String>, _flags: &StandardOptions) -> Result<(), SysexitsError> {
+pub fn fetch(urls: &Vec<String>, flags: &StandardOptions) -> Result<(), SysexitsError> {
     let mut output = stdout().lock();
 
     for url in urls {
-        match asimov_proxy::open(url) {
-            Ok(mut input) => {
-                std::io::copy(&mut input, &mut output)?;
+        if flags.verbose > 1 {
+            if url.contains(':') {
+                ceprintln!("<s><c>»</></> Fetching `{}`...", url);
+            } else {
+                ceprintln!("<s><c>»</></> Reading `{}`...", url);
             }
+        }
+
+        let mut input = match asimov_proxy::open(url) {
+            Ok(mut input) => input,
             Err(error) => {
-                eprintln!("failed to fetch URL `{}`: {}", url, error);
-                return Err(error.into());
+                let code = SysexitsError::from(&error);
+                let report: miette::Report = FetchError {
+                    url: url.clone(),
+                    cause: error,
+                }
+                .into();
+                eprintln!("{:?}", report);
+                return Err(code);
+            }
+        };
+
+        std::io::copy(&mut input, &mut output)?;
+
+        if flags.verbose > 0 {
+            if url.contains(':') {
+                ceprintln!("<s><g>✓</></> Fetched `{}`.", url); // TODO: time duration
+            } else {
+                ceprintln!("<s><g>✓</></> Read `{}`.", url);
             }
         }
     }
 
     Ok(())
+}
+
+#[derive(Debug, thiserror::Error, miette::Diagnostic)]
+#[error("Failed to fetch URL: {url}")]
+#[diagnostic(code("× Error"), url(docsrs))]
+struct FetchError {
+    url: String,
+
+    #[source]
+    #[diagnostic_source]
+    cause: asimov_proxy::Error,
 }
